f26b98eb61f11eaeaf96070884e872ee547ab809
Dmitry Ratnikov <dfr@beta.webitects.com>
2008-10-23 17:09:14 -0500

    Added rewritten javascript helper thingie.

--- app/helpers/javascript_helper.rb
+++ app/helpers/javascript_helper.rb
@@ -7,11 +7,17 @@ module JavascriptHelper
     "var book = constructBook(#{book.name})"
   end
 
-  def author_js_tag author
-    script_tag author_js(author)
+  instance_methods.select { |m| m =~ /_js$/ }.each do |js_method|
+    define_method "#{js_method}_tag" do |*args|
+      script_tag send(js_method, *args)
+    end
   end
 
-  def book_js_tag book
-    script_tag book_js(book)
-  end
+#  def author_js_tag author
+#    script_tag author_js(author)
+#  end
+#
+#  def book_js_tag book
+#    script_tag book_js(book)
+#  end
 end
